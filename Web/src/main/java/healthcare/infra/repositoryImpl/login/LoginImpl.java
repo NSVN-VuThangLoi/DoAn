f852f21b6ecfa9337e238ad3d4bec21fe9db106a
Vu Thang Loi <loi_vt@nittsusystem-vn.com>
2017-03-21 19:39:29 +0700

    update

--- Web/src/main/java/healthcare/infra/repositoryImpl/login/LoginImpl.java
+++ Web/src/main/java/healthcare/infra/repositoryImpl/login/LoginImpl.java
@@ -26,72 +26,6 @@ public class LoginImpl extends DataConnection implements LoginRepository{
 
 		@Override
 		public LoginDto findData(String userId) {
-			TypedQuery<HustInfoLogin> query = this.em.createQuery(FIND_DATA_USER, HustInfoLogin.class);
-			List<HustInfoLogin> entity = query.setParameter("userName", userName).getResultList();
-			if (entity.size() != 0) {
-				return convert(entity.get(0));
-			}
-			return null;
-		}
-
-		@Override
-		public void insertData(InfoDto dto) {
-			this.em.persist(convertEntity(dto));
-		}
-
-		@Override
-		public void updateData(InfoDto dto) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public void deleteData(InfoDto dto) {
-			// TODO Auto-generated method stub
-
-		}
-
-		@Override
-		public InfoDto findLogin(String userName, String passWord) {
-			TypedQuery<HustInfoLogin> query = this.em.createQuery(FIND_DATA_LOGIN, HustInfoLogin.class);
-			List<HustInfoLogin> entity = query.setParameter("userName", userName).setParameter("password", passWord)
-					.getResultList();
-			if (entity.size() != 0) {
-				return convert(entity.get(0));
-			}
-			return null;
-		}
-
-		@Override
-		public InfoDto findUserId(String userId) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		private InfoDto convert(HustInfoLogin entity) {
-			InfoDto dto = new InfoDto();
-			dto.setVersion(entity.getVersion());
-			dto.setUserName(entity.getUserName());
-			dto.setUserId(entity.getUserId());
-			dto.setStatus(entity.getStatus());
-			dto.setPassword(entity.getPassword());
-			dto.setAccessCode(entity.getAccessCode());
-			return dto;
-		}
-
-		private HustInfoLogin convertEntity(InfoDto dto) {
-			HustInfoLogin entity = new HustInfoLogin();
-			entity.setVersion(dto.getVersion());
-			entity.setUserName(dto.getUserName());
-			entity.setUserId(dto.getUserId());
-			entity.setStatus(dto.getStatus());
-			entity.setAccessCode(dto.getAccessCode());
-			entity.setPassword(dto.getPassword());
-			return entity;
-		}
-
-		@Override
-		public LoginDto findData(String name) {
 			// TODO Auto-generated method stub
 			return null;
 		}
@@ -126,7 +60,6 @@ public class LoginImpl extends DataConnection implements LoginRepository{
 			
 		}
 
-	}
-
+		
 
 }
